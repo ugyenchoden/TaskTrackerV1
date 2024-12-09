4010de8942398bfdc97b92d8ea38c25e984586ab
ugyenchoden <cugyen20@gmail.com>
2024-12-09 17:21:52 +1000

    feat(tasks): base service

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
   def index
+    tasks = Task.where(user_id: current_user.id)
+    render json: { tasks: tasks }
   end
 
   def show
+    render json: { task: task }
   end
 
   def create
@@ -11,12 +14,18 @@ class TasksController < ApplicationController
   end
 
   def update
+
   end
 
   def destroy
+    task.destroy!
   end
 
   private
+
+  def task
+    @task ||= Task.find(id)
+  end
 
   def task_params
     params.require(:task).permit(
