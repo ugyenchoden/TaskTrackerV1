afff654d5d9a82789a7d46f1c36596a9bb7722aa
ugyenchoden <cugyen20@gmail.com>
2024-12-09 13:00:48 +1000

    feat(tasks): base service

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,2 +1,21 @@
 class BaseService
+  include ActionView::Helpers::TranslationHelper
+  include Rails.application.routes.url_helpers
+  include ActiveSupport::Callbacks
+  include Assigner
+
+  attr_accessor :attributes, :current_user
+
+  define_callbacks :call
+
+  def initialize(attributes = {})
+    assign_attributes(attributes)
+  end
+
+  def call
+    run_callbacks :call do
+      yield if block_given?
+    end
+  end
+
 end
